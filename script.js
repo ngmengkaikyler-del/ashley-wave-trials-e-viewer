d949932cec514c0832a19988947533bf6002a8f6
ngmengkaikyler-del <ng_meng_kai_kyler@students.edu.sg>
2026-03-10 11:47:02 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
 async function loadLevel() {
-  const response = await fetch("level.json");
-  const data = await response.json();
 
-  document.getElementById("levelData").textContent =
-    JSON.stringify(data, null, 2);
+ const res = await fetch("level.json");
+ const level = await res.json();
+
+ const canvas = document.getElementById("levelCanvas");
+ const ctx = canvas.getContext("2d");
+
+ ctx.clearRect(0,0,canvas.width,canvas.height);
+
+ level.objects.forEach(o => {
+
+  if(o.type === "block"){
+    ctx.fillStyle = "white";
+    ctx.fillRect(o.x, 400-o.y, 40, 40);
+  }
+
+  if(o.type === "spike"){
+    ctx.fillStyle = "red";
+    ctx.beginPath();
+    ctx.moveTo(o.x,400-o.y);
+    ctx.lineTo(o.x+20,360-o.y);
+    ctx.lineTo(o.x+40,400-o.y);
+    ctx.closePath();
+    ctx.fill();
+  }
+
+  if(o.type === "saw"){
+    ctx.fillStyle = "pink";
+    ctx.beginPath();
+    ctx.arc(o.x,400-o.y,20,0,Math.PI*2);
+    ctx.fill();
+  }
+
+ });
+
 }
