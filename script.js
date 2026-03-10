759cd684056ffb27bace24d97944cff37260d3c2
ngmengkaikyler-del <ng_meng_kai_kyler@students.edu.sg>
2026-03-10 11:49:00 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-async function loadLevel() {
+async function loadLevel(){
 
  const res = await fetch("level.json");
  const level = await res.json();
@@ -6,32 +6,32 @@ async function loadLevel() {
  const canvas = document.getElementById("levelCanvas");
  const ctx = canvas.getContext("2d");
 
- ctx.clearRect(0,0,canvas.width,canvas.height);
+ level.objects.forEach(o=>{
 
- level.objects.forEach(o => {
-
-  if(o.type === "block"){
-    ctx.fillStyle = "white";
-    ctx.fillRect(o.x, 400-o.y, 40, 40);
+  if(o.type==="block"){
+   ctx.fillStyle="white";
+   ctx.fillRect(o.x,400-o.y,40,40);
   }
 
-  if(o.type === "spike"){
-    ctx.fillStyle = "red";
-    ctx.beginPath();
-    ctx.moveTo(o.x,400-o.y);
-    ctx.lineTo(o.x+20,360-o.y);
-    ctx.lineTo(o.x+40,400-o.y);
-    ctx.closePath();
-    ctx.fill();
+  if(o.type==="spike"){
+   ctx.fillStyle="red";
+   ctx.beginPath();
+   ctx.moveTo(o.x,400-o.y);
+   ctx.lineTo(o.x+20,360-o.y);
+   ctx.lineTo(o.x+40,400-o.y);
+   ctx.closePath();
+   ctx.fill();
   }
 
-  if(o.type === "saw"){
-    ctx.fillStyle = "pink";
-    ctx.beginPath();
-    ctx.arc(o.x,400-o.y,20,0,Math.PI*2);
-    ctx.fill();
+  if(o.type==="saw"){
+   ctx.fillStyle="pink";
+   ctx.beginPath();
+   ctx.arc(o.x,400-o.y,20,0,Math.PI*2);
+   ctx.fill();
   }
 
  });
 
 }
+
+loadLevel();
