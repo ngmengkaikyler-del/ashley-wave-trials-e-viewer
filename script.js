b0fcddf0045b89e0c4a256d49e5994e5ad313975
ngmengkaikyler-del <ng_meng_kai_kyler@students.edu.sg>
2026-03-10 11:49:47 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,38 @@
+let level;
+let scrollX = 0;
+let speed = 2;
+
 async function loadLevel(){
 
  const res = await fetch("level.json");
- const level = await res.json();
+ level = await res.json();
+
+ requestAnimationFrame(draw);
+
+}
+
+function draw(){
 
  const canvas = document.getElementById("levelCanvas");
  const ctx = canvas.getContext("2d");
 
+ ctx.clearRect(0,0,canvas.width,canvas.height);
+
  level.objects.forEach(o=>{
+
+  let x = o.x - scrollX;
 
   if(o.type==="block"){
    ctx.fillStyle="white";
-   ctx.fillRect(o.x,400-o.y,40,40);
+   ctx.fillRect(x,400-o.y,40,40);
   }
 
   if(o.type==="spike"){
    ctx.fillStyle="red";
    ctx.beginPath();
-   ctx.moveTo(o.x,400-o.y);
-   ctx.lineTo(o.x+20,360-o.y);
-   ctx.lineTo(o.x+40,400-o.y);
+   ctx.moveTo(x,400-o.y);
+   ctx.lineTo(x+20,360-o.y);
+   ctx.lineTo(x+40,400-o.y);
    ctx.closePath();
    ctx.fill();
   }
@@ -26,11 +40,15 @@ async function loadLevel(){
   if(o.type==="saw"){
    ctx.fillStyle="pink";
    ctx.beginPath();
-   ctx.arc(o.x,400-o.y,20,0,Math.PI*2);
+   ctx.arc(x,400-o.y,20,0,Math.PI*2);
    ctx.fill();
   }
 
  });
+
+ scrollX += speed;
+
+ requestAnimationFrame(draw);
 
 }
 
